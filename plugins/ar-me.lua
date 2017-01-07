c897ef7fef87d6246895a2d4da9ca89e9950d3ab
kekobot <vejegaz@9me.site>
2017-01-07 22:12:20 +0000

    Update ar-me.lua

--- plugins/ar-me.lua
+++ plugins/ar-me.lua
@@ -1,50 +1,56 @@
 do 
-local function jacky(msg, matches) 
-if issudo(msg) then 
-return '🎃 الايدي : '.. msg.from.id..'\n' 
-.'🎃 المعرف : @'..msg.from.username..'\n' 
-..'🎃 الاسم : '..msg.from.printname..'\n' 
-..'🎃 ايدي الكروب : '..msg.to.id..'\n' 
-..'🎃 اسم الكروب : '..msg.to.title..'\n' 
-..'🎃 موقعك : انــت تاج راسي حامي عرضي مطوري 🙈🎃' 
-end 
-if isowner(msg) then 
-  return '🎃 الايدي : '.. msg.from.id..'\n' 
-..'🎃 المعرف : @'..msg.from.username..'\n' 
-..'🎃 الاسم : '..msg.from.printname..'\n' 
-..'🎃 ايدي الكروب : '..msg.to.id..'\n' 
-..'🎃 اسم الكروب : '..msg.to.title..'\n' 
-..'🎃 موقعك : انــت الــمدير مال الــكــروب🎃❤️' 
-end 
+local function run(msg, matches) 
+ if issudo(msg) then 
+   local sudo = "عزيز موقعك ومعلوماتك هنا 👇🏌"..msg.from.firstname.."\n" 
+   .."🆔🎃 ايديك :("..msg.from.id..")\n" 
+   .."🆔🎃 ايدي الكروب :("..msg.to.id..")\n" 
+   .." 🎃  معرفك :(@"..(msg.from.username or "لا يوجد")..")\n" 
+   ..".🎃  رقمك :("..(msg.from.phone or " لا يوجد")..")\n" 
+   .."موقعك 🎃  : انته مطور مالتي 🕵👋 " 
+return replymsg(msg.id, sudo, okcb, true) 
+ end 
+ if isowner(msg) then 
+ local owner = "عزيزي موقعك ومعلوماتك هنا 👇🏌"..msg.from.firstname.."\n" 
+   .."🆔🎃 ايديك :("..msg.from.id..")\n" 
+   .."🆔🎃 ايدي الكروب :("..msg.to.id..")\n" 
+   .." 🎃  معرفك :(@"..(msg.from.username or "لا يوجد")..")\n" 
+   .."📞رقمك :("..(msg.from.phone or " لا يوجد ")..")\n" 
+   .."🎃 موقعك :انته مدير المجموعه 🚨 👥 »" 
+return replymsg(msg.id, owner, okcb, true) 
+ end 
+ if isadmin1(msg) then 
+local admin1 = "عزيزي موقعك ومعلوماتك هنا🎃 👇🏼" 
+   .."🆔🎃 ايديك :("..msg.from.id..")\n" 
+   .."🆔🎃 ايدي الكروب :("..msg.to.id..")\n" 
+   .."🎃  معرفك :(@"..(msg.from.username or "لا يوجد")..")\n" 
+   .." 🎃  رقمك :("..(msg.from.phone or " لايوجد ")..")\n" 
+   .."👷موقعك :انته ضلع المدير اقصد ادمن 🙋» " 
+return replymsg(msg.id, admin1, okcb, true) 
+end --@dev_90
 if ismomod(msg) then 
-return '🎃 الايدي : '.. msg.from.id..'\n' 
-.'🎃 المعرف : @'..msg.from.username..'\n' 
-..'🎃 الاسم : '..msg.from.printname..'\n' 
-..'🎃 ايدي الكروب : '..msg.to.id..'\n' 
-..'🎃 اسم الكروب : '..msg.to.title..'\n' 
-..'🎃 موقعك : انــت ادمــن🎃🤑' 
-end 
-if not ismomod(msg) then 
-return '🎃 الايدي : '.. msg.from.id..'\n' 
-..'.'🎃 المعرف : @'..msg.from.username..'\n' 
-..'🎃 الاسم : '..msg.from.printname..'\n' 
-..'🎃 ايدي الكروب : '..msg.to.id..'\n' 
-..'🎃 اسم الكروب : '..msg.to.title..'\n' 
-..'🎃 موقعك : انــت طفل قــصدي عــضو🎃😜' 
-end  
+      local admin = "عزيزي موقعك ومعلوماتك هنا 🎃 👇🏼" ..msg.from.firstname.. "\n" 
+   .."🆔🎃 ايديك :("..msg.from.id..")\n" 
+   .."🆔🎃 ايدي الكروب :("..msg.to.id..")\n" 
+   .."🎃 ️معرفك :(@"..(msg.from.username or "لا يوجد")..")\n" 
+   .."🎃 📞رقمك :("..(msg.from.phone or " لا يوجد ")..")\n" 
+   .."👮🎃 موقعك :انته ادمن المحترم 🎩" 
+return replymsg(msg.id, admin, okcb, true) 
+ end 
+ if not ismomod(msg) then 
+      local member = "عزيزي موقعك ومعلوماتك هنا🕵🎃 👇🏼" ..msg.from.firstname.. "\n" 
+   .."🆔🎃 ايديك :("..msg.from.id..")\n" 
+   .."🆔🎃 ايدي الكروب :("..msg.to.id..")\n" 
+   .."📌🎃 معرفك :(@"..(msg.from.username or "لا يوجد")..")\n" 
+   .."📞🎃 رقمك :("..(msg.from.phone or "لايوجد " )..")\n" 
+   .."🚸🎃 موقعك :انته طفل قصدي عضو 😹💔» " 
+return replymsg(msg.id, member, ok_cb, true) 
+ end 
+end --خاص لسورس كيكو لتصير مطي وتغير الحقوق
 
-if ( msg.text == "about" ) then 
-if ( msg.from.username == "#DEV: @keko11keko" ) then 
-R = getreceiver(msg) 
-sendlarge_msg ( R , "Made by @keko11keko" ); 
-end 
-end 
-end 
 return { 
   patterns = { 
        "^(موقعي)$", 
-
   }, 
-  run =jacky,
+  run = run, 
 } 
 end
