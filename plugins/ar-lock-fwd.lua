221a80637b0d63fb2d2321d244cae20146dec194
kekobot <vejegaz@9me.site>
2017-01-07 20:53:45 +0000

    Update ar-lock-fwd.lua

--- plugins/ar-lock-fwd.lua
+++ plugins/ar-lock-fwd.lua
@@ -1,51 +1,75 @@
---keko
 do
-local function pre_process(msg)
-
-    local fwd = 'mate:'..msg.to.id
-    if redis:get(fwd) and not is_momod(msg) and msg.fwd_from then
-            delete_msg(msg.id, ok_cb, true)
-             send_large_msg(get_receiver(msg),"سـتغفرآلله🙄🎃‏‏ جنآن هہ‏‏م دزو هہ‏‏م دزو ولگم مـتمـلون  لو آلله  خآلقني زآحف🐸 وآلآ هہ‏‏آيـه رآوبط  لگ مـتمـل🍼 يـا @"..msg.from.username)
-            return "ok"
-end
-        return msg
-    end
-    
- local function sadikal_knani10(msg, matches)
-     chat_id = msg.to.id
-local reply_id = msg['id']
-     if is_momod(msg) and matches[1]== 'قفل' and matches[2]== 'اعاده توجيه' then
-         local fwd = 'mate:'..msg.to.id
-         redis:set(fwd, true)
-         local text = '🎃 تم قفل 🎃 اعاده توجيه  '..'\n\n'..msg.to.title..'\nمن قبل 🎃 :-@'..msg.from.username..'\n'
-         return reply_msg(reply_id, text, ok_cb, false)
-         end
-local reply_id = msg['id']
-    if not is_momod(msg) and matches[1]== 'قفل' and matches[2]== 'اعاده توجيه' then
-    local text= '🎃  حبيبي لتلعب 👨‍💻'
- return reply_msg(reply_id, text, ok_cb, false)
-end
-local reply_id = msg['id']
-if is_momod(msg) and matches[1]== 'فتح' and matches[2]== 'اعاده توجيه' then
-    local fwd = 'mate:'..msg.to.id
-    redis:del(fwd)
-    local text = 'تم 🎃 فتح 🎃 اعاده توجيه'..'\n\n'..msg.to.title..'\nمن قبل :-@'..msg.from.username..'\n'
-    return reply_msg(reply_id, text, ok_cb, false)
-end
-
-local reply_id = msg['id']
-if not is_momod(msg) and matches[1]== 'فتح' and matches[2]== 'اعاده توجيه' then
-local text = '🎃  حبيبي لتلعب بكيفك👨‍💻'
- return reply_msg(reply_id, text, ok_cb, false)
+local function pre_process(msg) 
+local r = get_receiver(msg) 
+local debye = 'debye_fwd:'..msg.to.id
+local debye2 = 'debye2_fwd:'..msg.to.id
+local debye3 = 'debye3_fwd:'..msg.to.id
+local user = msg.from.id
+local chat = msg.to.id
+if redis:get(debye) and not is_momod(msg) and msg.fwd_from then
+    delete_msg(msg.id, ok_cb, true)
+elseif redis:get(debye2) and not is_momod(msg) and msg.fwd_from then
+delete_msg(msg.id, ok_cb, true)
+send_large_msg(get_receiver(msg), 'سـتغفرآلله🙄🎃‏‏ جنآن هہ‏‏م دزو هہ‏‏م دزو ولگم مـتمـلون  لو آلله  خآلقني زآحف🐸 وآلآ هہ‏‏آيـه رآوبط\nلگ مـتمـل🍼 يـا :: @'..msg.from.username)
+elseif redis:get(debye3) and not is_momod(msg) and msg.fwd_from then
+delete_msg(msg.id, ok_cb, false)
+ kick_user(user, chat)
+       return "done"
+       end
+   return msg
  end
-
+    
+    
+local function debye(msg, matches) 
+    local debye = msg['id'] 
+    chat_id =  msg.to.id 
+    if is_momod(msg) and matches[1] == 'حذف' and matches[2]== 'توجيه' then
+    local debye = 'debye_fwd:'..msg.to.id 
+    redis:set(debye, true)
+    local text = 'تم تفعيل حبيبي 😚سيتم حذف اتوجيه بدون تحذير😝🎃'
+    return reply_msg(msg.id, text, ok_cb, false)
+    
+ elseif is_momod(msg) and matches[1] == 'فتح حذف' and matches[2]== 'توجيه' then
+    local debye = 'debye_fwd:'..msg.to.id 
+    redis:del(debye)
+    local text = 'تم تعطيل قفل🐸 التوجيه بالحذف🎃'
+    return reply_msg(msg.id, text, ok_cb, false)   
+end
+if is_momod(msg) and matches[1] == 'تحذير' and matches[2]== 'توجيه' then
+    local debye2 = 'debye2_fwd:'..msg.to.id 
+    redis:set(debye2, true)
+    local text = 'تم تفعيل🏴 حذف + التحذير للتوجيه 🎃♥'
+    return reply_msg(msg.id, text, ok_cb, false)
+    
+ elseif is_momod(msg) and matches[1] == 'فتح تحذير' and matches[2]== 'توجيه' then
+    local debye2 = 'debye2_fwd:'..msg.to.id 
+    redis:del(debye2)
+    local text = 'تم تعطيل قفل⚠ التوجيه 🎃 بالتحذير 😍'
+    return reply_msg(msg.id, text, ok_cb, false)   
+end
+if is_momod(msg) and matches[1] == 'طرد' and matches[2]== 'توجيه' then
+    local debye3 = 'debye3_fwd:'..msg.to.id 
+    redis:set(debye3, true)
+    local text = 'تم تفعيل 🐸حذف  + طرد ♥🎃التوجيه'
+    return reply_msg(msg.id, text, ok_cb, false)
+    
+ elseif is_momod(msg) and matches[1] == 'فتح طرد' and matches[2]== 'توجيه' then
+    local debye3 = 'debye3_fwd:'..msg.to.id 
+    redis:del(debye3)
+    local text = 'تم تعطيل قفل التوجيه 🎃 بالطرد⚠🎃'
+    return reply_msg(msg.id, text, ok_cb, false)
+end
 end
 return {
-    patterns ={
-        '^(قفل) (اعاده توجيه)$',
-        '^(فتح) (اعاده توجيه)$'
+    patterns = {
+        '^(حذف) (توجيه)$',
+        '^(فتح حذف) (توجيه)$',
+        '^(تحذير) (توجيه)$',
+        '^(فتح تحذير) (توجيه)$',
+        '^(طرد) (توجيه)$',
+        '^(فتح طرد) (توجيه)$',
     },
-run = sadikal_knani10,
-pre_process = pre_process 
+    run = debye,
+    pre_process = pre_process
 }
 end
